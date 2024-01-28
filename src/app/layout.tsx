e5fa5e36b68b343b97e7c2020cf61e8c5bc1645f
willrockies <wlsn.ads@gmail.com>
2024-01-28 11:01:50 -0300

    navbar criado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import { Inter } from "next/font/google";
 import { Box, ThemeProvider } from "@mui/material";
-import theme from "./theme";
+import theme from "../theme";
+import { Navbar } from "@/components/Navbar/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Navbar />
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
           <ThemeProvider theme={theme}>
             <Box
