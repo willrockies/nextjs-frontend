d7ec02f0fbce35d6848cc383afd3cb898bf2e7a8
willrockies <wlsn.ads@gmail.com>
2024-02-02 17:34:53 -0300

    configurações, instalação de lib e montando pagina layout

---
+++ src/app/pagina1/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+
+function Pagina1(){
+    return(
+        <div>
+            <h1>pagina1</h1>
+            <Button variant="contained" color="primary">aaa</Button>
+        </div>
+    )
+}
+export default Pagina1;
